4676d18b455ed2203b36a3959a81d06949988873
Helena Zarazua <helenasky1993@gmail.com>
2021-03-29 14:15:37 -0700

    completed counting sort project

--- counting_sort_project/lib/counting_sort.js
+++ counting_sort_project/lib/counting_sort.js
@@ -1,5 +1,17 @@
 function countingSort(arr, max) {
-
+    let counter = new Array(max + 1).fill(0);
+    res = [];
+    
+    arr.forEach(num => {
+        counter[num]++;
+    })
+    counter.forEach((num, idx) => {
+        for(let i = num; i > 0; i--) {
+            res.push(idx);
+        }
+    })
+    return res;
+    
 }
 
 
