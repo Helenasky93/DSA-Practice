6efcba1d84e96ca53f4aff1f31f7e2295f3ded09
Helena Zarazua <helenasky1993@gmail.com>
2021-04-02 08:56:00 -0700

    completed stack project

--- stack_project/lib/stack.js
+++ stack_project/lib/stack.js
@@ -21,11 +21,60 @@
 // -----------
 
 class Node {
-
+    constructor(val) {
+        this.next = null;
+        this.value = val;
+    }
 }
 
 class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
 
+    //instertion
+    push(val) {
+        let node = new Node(val)
+        if (!this.top) {
+            this.top = node;
+            this.bottom = node;
+        } else {
+            let prevTop = this.top;
+            this.top = node;
+            this.top.next = prevTop;
+        }
+
+        this.length++;
+
+        return this.length;
+
+    }
+    //deletion
+    pop() {
+        let removed
+        if(!this.length) {
+            return null;
+        } else {
+            removed = this.top;
+            if(this.length === 1) {
+                this.top = null;
+                this.bottom = null;
+            } else{
+
+                this.top = this.top.next;
+            }
+
+        }
+        this.length--;
+        return removed.value;
+
+    }
+
+    size() {
+        return this.length;
+    }
 }
 
 exports.Node = Node;
