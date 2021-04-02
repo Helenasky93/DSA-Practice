254157f5aa35f9b244079058b809907b00712156
Helena Zarazua <helenasky1993@gmail.com>
2021-04-02 09:52:43 -0700

    completed queue project

--- queue_project/lib/queue.js
+++ queue_project/lib/queue.js
@@ -21,11 +21,75 @@
 // -----------
 
 class Node {
-
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
 }
 
 class Queue {
+    constructor() {
+        this.front = null;
+        this.back = null;
+        this.length = 0;
+    }
 
+    enqueue(val) {
+        let node = new Node(val);
+        if(!this.length) {
+            this.front = node;
+            this.back = node;
+        } else {
+            let oldBack = this.back;
+            this.back = node;
+            this.back.next = oldBack;
+            let temp = this.back;
+            // console.log("temp.next", temp);
+            while (temp) {
+                if (temp.next === this.front) {
+                    this.front.next = temp
+                    // this.front.next = null;
+                    break
+                }
+                temp = temp.next
+            }
+
+        }
+        this.length++;
+        return this.length;
+
+    }
+
+    dequeue() {
+        let oldFront;
+        if (!this.length) {
+            return null;
+        } else {
+            oldFront = this.front;
+            if(this.length === 1) {
+                this.front = null;
+                this.back = null;
+            } else {
+                let temp = this.back;
+                // console.log("temp.next", temp);
+                while(temp) {
+                    if (temp.next === oldFront) {
+                        this.front = temp;
+                        // this.front.next = null;
+                        break
+                    }
+                    temp = temp.next
+                }
+            }
+        }
+        this.length--;
+        return oldFront.value;
+
+    }
+
+    size() {
+        return this.length;
+    }
 }
 
 exports.Node = Node;
